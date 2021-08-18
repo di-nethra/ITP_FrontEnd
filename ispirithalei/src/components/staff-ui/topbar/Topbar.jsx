005dd2e5e042bac3852589cf76461a668cedf74a
danushka9999 <dranasinghe99@gmail.com>
2021-08-18 23:15:12 +0530

    Top bar final implementation

--- ispirithalei/src/components/staff-ui/topbar/Topbar.jsx
+++ ispirithalei/src/components/staff-ui/topbar/Topbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./topbar.css"
+import ISPIRITHALEI from "../../../assets/2.png";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default function Topbar() {
     return (
-        <div>
-            Topbar
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <a className="logo" href="/">
+                        <img src={ISPIRITHALEI} alt="brandLogo" className="img-logo" />
+                    </a>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <AccountCircleIcon fontSize="large"/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
