bdefab53daa6028104d9b65c437ca06bdfee2764
danushka9999 <dranasinghe99@gmail.com>
2021-08-20 13:19:33 +0530

    Update App.js

--- ispirithalei/src/App.js
+++ ispirithalei/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import LabAssitant from './layouts/Labasisstant'
-
-function App() {
-    return (
-        <div>
-           <LabAssitant /> 
-        </div>
-    )
-}
+const App = () => (
+    <div>
+        
+    </div>
+)
 
 export default App
