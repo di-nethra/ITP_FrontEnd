9e46191cecbbcd9d750880fb66cb6e83a3b9da33
AmanMU <IT20155520@my.sliit.lk>
2021-08-22 03:01:53 +0530

    Added Footer to patient portal

---
+++ ispirithalei/src/components/patient-ui/Footer/FooterAddress.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Typography} from "@material-ui/core";
+
+export default function FooterAddress() {
+    return (
+        <div>
+            <Typography variant={"h5"}>
+                Find our Hospital
+            </Typography>
+                <Typography variant={"body1"}>
+                    647, Utuwakanda,Mawanella.
+                </Typography>
+            <Typography variant={"body1"}>
+                https://goo.gl/maps/7DCNYV14c5mrUjHu
+            </Typography>
+        </div>
+    )
+}
