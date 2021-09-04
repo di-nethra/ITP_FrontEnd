7548f527cded6fc51b30384f29c233c20f27b771
di-nethra <anjana.dinethra@gmail.com>
2021-09-04 19:42:50 +0530

    creditCard page added

---
+++ ispirithalei/src/pages/patient-ui/CreditCardPage.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import { useState } from "react";
+import logo from "../../assets/images/infoPageLogo.svg";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import PayCredit from "../../assets/images/PayWithCreditCard.svg";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+import DropDown from "../../components/patient-ui/Payments/DropDown";
+const styles = (theme) => ({
+  infoLogo: {
+    width: "500px",
+    marginLeft: "200px",
+    marginBottom: "1000px",
+  },
+  text: {
+    textAlign: "center",
+    paddingLeft: "350px",
+  },
+  label: {
+    textAlign: "left",
+    marginTop: "20px",
+  },
+
+  TextField: {
+    marginTop: "20px",
+    textAlign: "right",
+  },
+  TextField1: {
+    marginTop: "20px",
+    textAlign: "left",
+  },
+  Paybutton: {
+    marginTop: "20px",
+    width: "400px",
+    height: "50px",
+    background: "#3C4257",
+    "&:hover": {
+      color: "black",
+      backgroundColor: "#D9FAFF",
+    },
+  },
+});
+
+export class CreditCardPage extends Component {
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { values, handleChange } = this.props;
+    console.log(values);
+    console.log(values.email);
+    let data = [{ email: values.email }, { id: 2 }];
+    return (
+      <div>
+        <Grid container direction="row" className={classes.root}>
+          <Grid item md={4}>
+            <img src={logo} alt="40" className={classes.infoLogo} />
+          </Grid>
+          <Grid direction="column" md={4} className={classes.text}>
+            <Grid item>
+              <img src={PayCredit} alt="70" />
+            </Grid>
+
+            <form noValidate>
+              <p className={classes.label}>email</p>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth="true"
+                onChange={handleChange("email")}
+                defaultValue={values.email}
+              />
+              <p className={classes.label}>Card Number</p>
+              <TextField
+                id="outlined-basic"
+                label="Card Number"
+                variant="outlined"
+                fullWidth="true"
+                onChange={handleChange("cardNumber")}
+                defaultValue={values.cardNumber}
+              />
+              <Grid item direction="row">
+                <Grid item className={classes.TextField1}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Year/Month"
+                    variant="outlined"
+                    onChange={handleChange("yearMonth")}
+                    defaultValue={values.yearMonth}
+                  />
+                  <TextField
+                    id="outlined-basic"
+                    label="CVC"
+                    variant="outlined"
+                    onChange={handleChange("CVC")}
+                    defaultValue={values.CVC}
+                  />
+                </Grid>
+              </Grid>
+              <p className={classes.label}>Name on the Card</p>
+              <TextField
+                id="outlined-basic"
+                label="Name on the Card"
+                variant="outlined"
+                fullWidth="true"
+                onChange={handleChange("nameOnTheCard")}
+                defaultValue={values.nameOnTheCard}
+              />
+              <p className={classes.label}>Country</p>
+
+              <DropDown onChange={handleChange("country")} />
+            </form>
+
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.Paybutton}
+              onClick={this.continue}
+            >
+              Proceed to Checkout
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(CreditCardPage);
