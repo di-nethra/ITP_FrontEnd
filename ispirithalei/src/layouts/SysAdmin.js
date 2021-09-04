1275a51576e78deb8b22cc9e575437ed63b1a210
Chamod-G <IT20409982@my.sliit.lk>
2021-09-04 14:48:06 +0530

    Sys admin initial

---
+++ ispirithalei/src/layouts/SysAdmin.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Sidebar from "../components/staff-ui/sidebar/Sidebar";
+import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
+import PublishOutlinedIcon from '@material-ui/icons/PublishOutlined';
+import TransformIcon from '@material-ui/icons/Transform';
+import DoneIcon from '@material-ui/icons/Done';
+import ClearAllIcon from '@material-ui/icons/ClearAll';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import Topbar from "../components/staff-ui/topbar/Topbar";
+import "./layout.css"
+
+const SysAdmin = () => {
+  const user = {
+    name : 'Chamod Gavindya',
+    role : 'Sys Admin',
+    list : [
+      {
+        path: "",
+        exact: true,
+        icon: <DashboardOutlinedIcon className="sidebarIcon"/>,
+        iconlabel: 'Dashboard',
+        id: 1
+      },
+      {
+        path: "/pendingappointments",
+        icon: <PublishOutlinedIcon className="sidebarIcon"/>,
+        iconlabel: 'Employee List',
+        id: 2
+      },
+      {
+        path: "/checkedinappointments",
+        icon: <TransformIcon className="sidebarIcon"/>,
+        iconlabel: 'Login credentials',
+        id: 3
+      },
+      {
+        path: "/allappointments",
+        icon: <DoneIcon className="sidebarIcon"/>,
+        iconlabel: 'Payroll',
+        id: 4
+      },
+      
+
+    ]
+  }
+  
+  return(
+  <div >
+    <div className="container">
+
+      <Sidebar user={user}/>
+      <div className="others">
+          <Topbar page={user.list}/>
+          {/* <LabassHome/> */}
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default SysAdmin;
