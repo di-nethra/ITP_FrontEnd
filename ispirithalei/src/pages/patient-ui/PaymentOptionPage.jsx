17edb18ac2275e3a73a50ecde7dd31acca5be8a6
di-nethra <anjana.dinethra@gmail.com>
2021-09-04 19:28:08 +0530

    payment option page added

---
+++ ispirithalei/src/pages/patient-ui/PaymentOptionPage.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import CreditCard from "../../assets/images/CreditcardLogo.svg";
+import MobilePay from "../../assets/images/MobilePayLogo.svg";
+import StripePay from "../../assets/images/StripePayLogo.svg";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  logo: {
+    marginTop: "100px",
+    paddingLeft: "100px",
+    marginBottom: "1000px",
+  },
+  option: {
+    "&:hover": {
+      background: "#D9FAFF",
+    },
+  },
+});
+
+function PaymentOptionPage() {
+  const classes = useStyles();
+  const history = useHistory();
+  return (
+    <div>
+      <Grid container className={classes.logo}>
+        <Grid item xs={12} md={4}>
+          <a href="#">
+            <img
+              src={CreditCard}
+              alt="60"
+              className={classes.option}
+              onClick={() => history.push("/info")}
+            />
+          </a>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <a href="#">
+            <img src={MobilePay} alt="60" className={classes.option} />
+          </a>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <a href="#">
+            <img src={StripePay} alt="60" className={classes.option} />
+          </a>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default PaymentOptionPage;
