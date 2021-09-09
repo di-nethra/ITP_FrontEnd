2d394b6523e74d5e6ca21edddc54a55629c24e72
AmanMU <IT20155520@my.sliit.lk>
2021-09-09 20:11:48 +0530

    added animation to patient homepage

--- ispirithalei/src/components/patient-ui/Home/Service.js
+++ ispirithalei/src/components/patient-ui/Home/Service.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Box, Typography, useTheme} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import Fade from 'react-reveal/Fade';
 
 function Service(props) {
     const theme = useTheme();
@@ -18,29 +19,40 @@ function Service(props) {
         },
 
         heading: {
-            padding: '10px 50px',
+            padding: '10px 40px',
         },
 
         description: {
-            padding: '10px 50px',
+            padding: '5px 40px',
             textAlign: 'justify',
-            overflowWrap: 'break-word'
+            textJustify: "inter-character"
         }
     });
     const style = useStyles();
 
     return (
+        <Fade bottom cascade>
         <div>
+
             <Box className={style.card}>
                 <img height="150px" src={props.img} alt="Service Provided by our system"/>
             </Box>
+            <Fade bottom cascade delay={300}>
             <Typography variant={"h5"} className={style.heading}>
+
                 {props.heading}
+
             </Typography>
+            </Fade>
+            <Fade bottom cascade delay={300}>
             <Typography className={style.description}>
+
                 {props.description}
+
             </Typography>
+            </Fade>
         </div>
+</Fade>
     )
 };
 
