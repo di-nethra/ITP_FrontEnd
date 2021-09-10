b8f594b0cf76aa7b3dd187a30d13b7d8e56f9dba
Chamod-G <IT20409982@my.sliit.lk>
2021-09-10 19:00:30 +0530

    changes

--- ispirithalei/src/App.js
+++ ispirithalei/src/App.js
@@ -1,79 +1,75 @@
-import React from 'react'
+import React from "react";
 import Header from "./components/patient-ui/Header/Header";
 import Home from "./pages/patient-ui/Home";
-import {createTheme, ThemeProvider} from "@material-ui/core";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./pages/patient-ui/About";
 import Labasisstant from "./layouts/Labasisstant";
 import Receptionist from "./layouts/Receptionist";
 import Footer from "./components/patient-ui/Footer/Footer";
 import Login from "./components/staff-ui/Login";
-import Doctor from "./layouts/Doctor"
-import Inventory from './layouts/Inventory';
-import SysAdmin from './layouts/SysAdmin';
-import EForm from './pages/patient-ui/ChanellForm';
-
+import Doctor from "./layouts/Doctor";
+import Inventory from "./layouts/Inventory";
+import SysAdmin from "./layouts/SysAdmin";
+import EForm from "./pages/patient-ui/ChanellForm";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#005792',
-            dark: '#00204A',
-            light: '#D9FAFF',
-        },
+  palette: {
+    primary: {
+      main: "#005792",
+      dark: "#00204A",
+      light: "#D9FAFF",
     },
+  },
 
-    typography: {
-        fontFamily: 'Poppins',
-        fontWeightLight: 400,
-        fontWeightRegular: 500,
-        fontWeightMedium: 600,
-        fontWeightBold: 700,
-    },
-    shape : {
-        borderRadius: '15px'
-    },
+  typography: {
+    fontFamily: "Poppins",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+  shape: {
+    borderRadius: "15px",
+  },
 });
 
 const App = () => (
-    <ThemeProvider theme={theme}>
-        <Router>
-            <Switch>
-                <Route path={["/staff/inventorymanager", "/staff/receptionist", "/login","/staff/doctor", "/staff/sysadmin", "/staff/labassistant"]}>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Switch>
+        <Route
+          path={[
+            "/staff/inventorymanager",
+            "/staff/receptionist",
+            "/login",
+            "/staff/doctor",
+            "/staff/sysadmin",
+            "/staff/labassistant",
+          ]}
+        >
+          <Switch>
+            <Route path="/staff/inventorymanager" component={Inventory} />
+            <Route path="/staff/receptionist" component={Receptionist} />
+            <Route path="/login" component={Login} />
+            <Route path="/staff/labassistant" component={Labasisstant} />
+            <Route path="/staff/doctor" component={Doctor} />
+            <Route path="/staff/sysadmin" component={SysAdmin} />
+          </Switch>
+        </Route>
 
-                    <Switch>
-                        <Route path="/staff/inventorymanager" component={Inventory} />
-                        <Route path="/staff/receptionist" component={Receptionist} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/staff/labassistant" component={Labasisstant} />
-                        <Route path="/staff/doctor" component={Doctor} />
-                        <Route path="/staff/sysadmin" component={SysAdmin} />
+        <Route path={["/patient/newappointment", "/about", "/"]}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
 
-                    </Switch>
-
-                </Route>
-
-                  
-            
-
-
-                <Route path={["/patient/newappointment","/about", "/"]}>
-                    <Header />
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/about" component={About} />
-
-                        <Route path="/patient/newappointment" component={EForm} />
-
-                    </Switch>
-                    <Footer />
-                </Route>
-            </Switch>
-        </Router>
-    </ThemeProvider>
-)
-export default App
+            <Route path="/patient/newappointment" component={EForm} />
+          </Switch>
+          <Footer />
+        </Route>
+      </Switch>
+    </Router>
+  </ThemeProvider>
+);
+export default App;
--- ispirithalei/src/pages/staff-ui/sysadmin/FormDetails.js
+++ ispirithalei/src/pages/staff-ui/sysadmin/FormDetails.js
@@ -1,177 +1,174 @@
-import React, {Component} from 'react';
-import { MuiThemeProvider } from '@material-ui/core';
-import { TextField } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import empFormService from '../../../services/empForm.service';
-import Role from './Role';
-import { Typography } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import CardContent from '@material-ui/core/CardContent';
-import { Card } from '@material-ui/core';
-import SendIcon from '@material-ui/icons/Send';
-import AutorenewIcon from '@material-ui/icons/Autorenew';
+import React, { Component } from "react";
+import { MuiThemeProvider } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import empFormService from "../../../services/empForm.service";
+import Role from "./Role";
+import { Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import CardContent from "@material-ui/core/CardContent";
+import { Card } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import AutorenewIcon from "@material-ui/icons/Autorenew";
 
- 
-export class FormDetails extends Component{
-    
-    continue = e => {
-        e.preventDefault();
-        this.props.nextStep();
+export class FormDetails extends Component {
+  continue = (e) => {
+    e.preventDefault();
+    this.props.nextStep();
 
-        const data = {
-            firstName: this.props.values.firstName,
-            lastName: this.props.values.lastName,
-            email: this.props.values.email,
-            mobile: this.props.values.mobile,
-            address: this.props.values.address,
+    const data = {
+      firstName: this.props.values.firstName,
+      lastName: this.props.values.lastName,
+      email: this.props.values.email,
+      mobile: this.props.values.mobile,
+      address: this.props.values.address,
+    };
 
-          };
+    console.log(this.props.values);
 
-        console.log(this.props.values);
+    console.log(data.email);
+    empFormService
+      .create(data)
 
-        console.log(data.email);
-        empFormService
-        .create(data)
-    
-        .then((response) => {
-            alert("success");
-            console.log("inside create" + response.data);
-            console.log("inside then" + response.data);
-        })
-        .catch((e) => {
-            // alert(e );
-            console.log("this is the error:" + e);
-        });
-    
-        this.props.nextStep();
-    }
+      .then((response) => {
+        alert("success");
+        console.log("inside create" + response.data);
+        console.log("inside then" + response.data);
+      })
+      .catch((e) => {
+        // alert(e );
+        console.log("this is the error:" + e);
+      });
 
-    render(){
-        
-        const {values, handleChange} = this.props;
-        return (
+    this.props.nextStep();
+  };
 
-            <div>
-                <MuiThemeProvider >
-                    <React.Fragment >
-                        <Card>
-                            <CardContent>
-                                <Typography variant="h6" gutterBottom>
-                                    Employee Form
-                                </Typography>
-                                <br></br>
-                                <br></br>
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <div>
+        <MuiThemeProvider>
+          <React.Fragment>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" gutterBottom>
+                  Employee Form
+                </Typography>
+                <br></br>
+                <br></br>
 
-                                <Grid container spacing={3}>
-                                    <Grid item xs={4}>
-                                        <p style={styles.label}>Role</p>
-                                    </Grid>
-                                    <Grid item xs={5}>
-                                        <Role />
-                                    </Grid>
-                                    
-                                    <Grid item xs={4}>
-                                        <p style={styles.label}>First Name</p>
-                                    </Grid>
-                                    <Grid item xs={5}>
-                                        <TextField 
-                                            label="First Name"
-                                            variant="outlined"
-                                            fullWidth={true}
-                                            onChange={handleChange('firstName')}
-                                            defaultValue={values.firstName}
-                                        />
-                                    </Grid>
-                                    
-                                    <Grid item xs={4}>
-                                        <p style={styles.label}>Last Name</p>
-                                    </Grid>
-                                    <Grid item xs={5}>
-                                        <TextField 
-                                            label="Last Name"
-                                            variant="outlined"
-                                            fullWidth={true}
-                                            onChange={handleChange('lastName')}
-                                            defaultValue={values.lastName}
-                                        />
-                                    </Grid>
-                                    
-                                    <Grid item xs={4}>
-                                        <p style={styles.label}>Email</p>
-                                    </Grid>
-                                    <Grid item xs={5}>
-                                        <TextField 
-                                            label="Email"
-                                            variant="outlined"
-                                            fullWidth={true}
-                                            onChange={handleChange('email')}
-                                            defaultValue={values.email}
-                                        />
-                                    </Grid>
+                <Grid container spacing={3}>
+                  <Grid item xs={4}>
+                    <p style={styles.label}>Role</p>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <Role />
+                  </Grid>
 
-                                    <Grid item xs={4}>
-                                        <p style={styles.label}>Mobile</p>
-                                    </Grid>
-                                    <Grid item xs={5}>
-                                        <TextField 
-                                            label="Mobile"
-                                            variant="outlined"
-                                            fullWidth={true}
-                                            onChange={handleChange('mobile')}
-                                            defaultValue={values.email}
-                                        />
-                                    </Grid>
-                                    
-                                    <Grid item xs={4}>
-                                        <p style={styles.label}>Address</p>
-                                    </Grid>
-                                    <Grid item xs={5}>
-                                        <TextField 
-                                            label="Adress"
-                                            variant="outlined"
-                                            fullWidth={true}
-                                            onChange={handleChange('address')}
-                                            defaultValue={values.email}
-                                        />
-                                    </Grid>
+                  <Grid item xs={4}>
+                    <p style={styles.label}>First Name</p>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="First Name"
+                      variant="outlined"
+                      fullWidth={true}
+                      onChange={handleChange("firstName")}
+                      defaultValue={values.firstName}
+                    />
+                  </Grid>
 
-                                    <Grid item xs={12}>
-                                        <Button 
-                                        variant="contained"
-                                        color="secondary"
-                                        style={styles.button}
-                                        startIcon={<AutorenewIcon />}
-                                        >Reset</Button>
+                  <Grid item xs={4}>
+                    <p style={styles.label}>Last Name</p>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="Last Name"
+                      variant="outlined"
+                      fullWidth={true}
+                      onChange={handleChange("lastName")}
+                      defaultValue={values.lastName}
+                    />
+                  </Grid>
 
-                                        <Button 
-                                        variant="contained"
-                                        color="primary"
-                                        style={styles.button}
-                                        startIcon={<SendIcon />}
-                                        onClick={this.continue}
-                                        >Submit</Button>
-                                    </Grid>
+                  <Grid item xs={4}>
+                    <p style={styles.label}>Email</p>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="Email"
+                      variant="outlined"
+                      fullWidth={true}
+                      onChange={handleChange("email")}
+                      defaultValue={values.email}
+                    />
+                  </Grid>
 
-                                </Grid>
-                            </CardContent>
-                        </Card>
-                    </React.Fragment>
-                </MuiThemeProvider>
-            </div>
-        )
-    }
+                  <Grid item xs={4}>
+                    <p style={styles.label}>Mobile</p>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="Mobile"
+                      variant="outlined"
+                      fullWidth={true}
+                      onChange={handleChange("mobile")}
+                      defaultValue={values.email}
+                    />
+                  </Grid>
+
+                  <Grid item xs={4}>
+                    <p style={styles.label}>Address</p>
+                  </Grid>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="Adress"
+                      variant="outlined"
+                      fullWidth={true}
+                      onChange={handleChange("address")}
+                      defaultValue={values.email}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      style={styles.button}
+                      startIcon={<AutorenewIcon />}
+                    >
+                      Reset
+                    </Button>
+
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      style={styles.button}
+                      startIcon={<SendIcon />}
+                      onClick={this.continue}
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </React.Fragment>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    button:{
-        margin: 20,
-        left: 415,
-    },
+  button: {
+    margin: 20,
+    left: 415,
+  },
 
-    label:{
-        margin: 30,
-    }
-
-}
+  label: {
+    margin: 30,
+  },
+};
 
 export default FormDetails;
