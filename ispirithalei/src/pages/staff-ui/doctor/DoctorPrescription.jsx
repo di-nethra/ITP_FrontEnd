66a9177bd09c7de811b0b144f64158e359900f4b
Asel <asel.jaya@gmail.com>
2021-09-11 18:14:48 +0530

    removed all unused import statements

--- ispirithalei/src/pages/staff-ui/doctor/DoctorPrescription.jsx
+++ ispirithalei/src/pages/staff-ui/doctor/DoctorPrescription.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import DoctorHome from "./DoctorHome";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
