52f65d0df923fd154899863af4e3e7c8bd5b03b3
Madsanka <mwattale@gmail.com>
2021-09-19 00:59:36 +0530

    Patient prescription table

--- src/pages/staff-ui/pharmacist/PharmacistHome.jsx
+++ src/pages/staff-ui/pharmacist/PharmacistHome.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'patientname', headerName: 'Patient Name', width: 280 },
+  { field: 'diagnosis', headerName: 'Diagnosis', width: 280},
+  { field: 'action', headerName: 'Action',type:'button', width: 180},
+];
+
+const rows = [
+ // { id: 1, patientname: 'Snow', diagnosis: 'Jon', action: 35 },
+ // { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+ // { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+ // { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+];
 
 export default function PharmacistHome() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div style={ {marginTop: 20, marginLeft: 40 } }>
+    <h3>PATIENT PRESCRIPTION</h3>
+    <div style={{ height: 400, width: '80%', marginLeft: 80, marginTop: 40}}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+      />
+    </div>
+    </div>
+  );
 }
