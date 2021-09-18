79d0d93a79fff535c9937bac1b3b338f31bd7642
danushka9999 <dranasinghe99@gmail.com>
2021-09-19 01:29:42 +0530

    set the subbmitted test count

--- src/components/staff-ui/featuredInfo/FeaturedInfo.jsx
+++ src/components/staff-ui/featuredInfo/FeaturedInfo.jsx
@@ -2,14 +2,34 @@ import "./featuredInfo.css";
 import DoneOutlinedIcon from '@material-ui/icons/DoneOutlined';
 import TransformOutlinedIcon from '@material-ui/icons/TransformOutlined';
 import ArrowUpwardOutlinedIcon from '@material-ui/icons/ArrowUpwardOutlined';
+import {useEffect, useState} from 'react';
+import TestDataService from "../../../services/tests.service";
 
 export default function FeaturedInfo() {
+
+  const [subb, setSubb] = useState([]);
+  useEffect(() => {
+    retieveSubbmittedTests();
+  }, []);
+
+  const retieveSubbmittedTests = () => {
+    TestDataService.getAllSubbmited()
+        .then(response => {
+          setSubb(response.data)
+        })
+        .catch(err => {
+              console.log("Error while getting data from database" + err);
+            }
+        )
+  };
+  const subbmitted = subb.length;
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Subbmitted Tests</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">5</span>
+          <span className="featuredMoney">{subbmitted}</span>
           <span className="featuredMoneyRate">
              <ArrowUpwardOutlinedIcon  className="featuredIcon"/>
           </span>
