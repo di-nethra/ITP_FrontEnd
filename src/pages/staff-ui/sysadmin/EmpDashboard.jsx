f1f186168ab4edad938599f722080432b654277d
Chamod-G <IT20409982@my.sliit.lk>
2021-09-18 19:22:24 +0530

    populated the dashboard data grid

--- src/pages/staff-ui/sysadmin/EmpDashboard.jsx
+++ src/pages/staff-ui/sysadmin/EmpDashboard.jsx
@@ -1,53 +1,75 @@
 import * as React from "react";
 import { DataGrid } from "@material-ui/data-grid";
+import empFormService from "../../../services/empForm.service";
+import { useEffect, useState } from "react";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { 
+    field: "role", 
+    headerName: "Role", 
+    width: 150 
+},
   {
     field: "firstName",
     headerName: "First name",
-    width: 150,
+    width: 155,
     editable: true,
   },
   {
     field: "lastName",
     headerName: "Last name",
-    width: 150,
+    width: 155,
     editable: true,
   },
   {
-    field: "age",
-    headerName: "Age",
+    field: "mobile",
+    headerName: "Mobile",
     type: "number",
-    width: 110,
+    width: 140,
     editable: true,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, "firstName") || ""} ${
-        params.getValue(params.id, "lastName") || ""
-      }`,
+    field: "address",
+    headerName: "Address",
+    width: 155,
+    editable: true,
   },
+
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
 
 function EmpDashboard() {
+  const [details, setDetails] = useState([]);
+  useEffect(() => {
+    getDetails();
+  },[]);
+
+
+
+  const getDetails = () => {
+    empFormService
+      .getAll()
+      .then((response) => {
+        setDetails(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  let rows = [];
+  for (const detail of details) {
+    rows.push({
+      id:detail._id,
+      role: detail.role,
+      firstName: detail.firstName,
+      lastName: detail.lastName,
+      email: detail.email,
+      mobile: detail.mobile,
+      address: detail.address,
+    });
+  }
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
@@ -56,6 +78,7 @@ function EmpDashboard() {
         pageSize={5}
         checkboxSelection
         disableSelectionOnClick
+        
       />
     </div>
   );
