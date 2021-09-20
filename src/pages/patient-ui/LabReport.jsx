cc435d09f78c1d88b2d289bc09ecbe7c24cd4b25
danushka9999 <dranasinghe99@gmail.com>
2021-09-20 17:35:30 +0530

    warning removel

--- src/pages/patient-ui/LabReport.jsx
+++ src/pages/patient-ui/LabReport.jsx
@@ -1,9 +1,9 @@
 import labreport from '../../assets/images/labreport.svg'
 import './labreport.css'
 import TestDataService from '../../services/tests.service';
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Swal from "sweetalert2";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 export default function LabReport() {
 
     let history = useHistory();
@@ -21,74 +21,73 @@ export default function LabReport() {
     const submitTest = (e) => {
         e.preventDefault();
         //const data = {
-           // specimenid: specimenid,
-           // contactnumber: contactnumber,
+        // specimenid: specimenid,
+        // contactnumber: contactnumber,
         //};
 
-        TestDataService.findByCNandID(contactnumber,specimenid)
+        TestDataService.findByCNandID(contactnumber, specimenid)
             .then((response) => {
-                const data = response.data
-                    if (response.status === 200) { 
-                        //console.log(response.data._id);   
-                        history.push("/labreport/download/"+response.data._id);
-                    }
-                
+                //const data = response.data
+                if (response.status === 200) {
+                    //console.log(response.data._id);   
+                    history.push("/labreport/download/" + response.data._id);
                 }
+
+            }
             )
-            .
-                catch((e) => {
-                    Swal.fire(
-                        "Test Not Found",
-                        "Please Check the bill and enter correct specimen id and contact number",
-                        "error"
-                      );
-                    // alert(e);
-                    console.log("ERROR : " + e);
-                });
-            };
+            .catch((e) => {
+                Swal.fire(
+                    "Test Not Found",
+                    "Please Check the bill and enter correct specimen id and contact number",
+                    "error"
+                );
+                // alert(e);
+                console.log("ERROR : " + e);
+            });
+    };
 
-    
+
     return (
-            <div class="detailsbock11">
-                <span className="newUserTitle11">Lab Reports</span><br></br>
-                <div class="detailsbock12">
-                    <form className="newUserForm11" onSubmit={submitTest}>
-                        <div className="newUserItem11">
-                            <label>Specimen ID(Printed on bill)</label>
-                            <input
-                                name="specimenid"
-                                required
-                                type="text"
-                                minLength="3"
-                                maxLength="3"
-                                value={specimenid}
-                                placeholder="Ex:100"
-                                onChange={handleInputChangespecimenid}
-                            />
-                        </div>
-                        <div className="newUserItem11">
-                            <label>Mobile Number(Printed on bill)</label>
-                            <input
+        <div class="detailsbock11">
+            <span className="newUserTitle11">Lab Reports</span><br></br>
+            <div class="detailsbock12">
+                <form className="newUserForm11" onSubmit={submitTest}>
+                    <div className="newUserItem11">
+                        <label>Specimen ID(Printed on bill)</label>
+                        <input
+                            name="specimenid"
+                            required
+                            type="text"
+                            minLength="3"
+                            maxLength="3"
+                            value={specimenid}
+                            placeholder="Ex:100"
+                            onChange={handleInputChangespecimenid}
+                        />
+                    </div>
+                    <div className="newUserItem11">
+                        <label>Mobile Number(Printed on bill)</label>
+                        <input
 
-                                name="contactnumber"
-                                type="text"
-                                required
-                                minLength="10"
-                                maxLength="10"
-                                placeholder="Enter mobile number"
-                                value={contactnumber}
-                                onChange={handleInputChangecontact}
+                            name="contactnumber"
+                            type="text"
+                            required
+                            minLength="10"
+                            maxLength="10"
+                            placeholder="Enter mobile number"
+                            value={contactnumber}
+                            onChange={handleInputChangecontact}
 
-                            />
-                        </div>
+                        />
+                    </div>
 
-                        <button className="newUserButton11">Get Lab Reports</button>
-                    </form>
-                    <img src={labreport} alt="labreports" style={{ height: 400, width: 400 }} class="imglab"/>
-                </div>
-                
+                    <button className="newUserButton11">Get Lab Reports</button>
+                </form>
+                <img src={labreport} alt="labreports" style={{ height: 400, width: 400 }} class="imglab" />
             </div>
-        
+
+        </div>
+
 
     )
 }
