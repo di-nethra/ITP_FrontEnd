29fefa906d30137d7f2d295204d4ef7606be8173
AmanMU <IT20155520@my.sliit.lk>
2021-10-05 15:08:53 +0530

    added skeloton loading for receptionist Dashboard.jsx

--- src/pages/staff-ui/receptionist/Dashboard.jsx
+++ src/pages/staff-ui/receptionist/Dashboard.jsx
@@ -3,8 +3,8 @@ import "./Receptionist.css"
 import {Card, CardContent, CardHeader, Grid} from "@material-ui/core";
 import channellServices from "../../../services/echannelling.Service";
 import {Link} from "react-router-dom";
-import {Skeleton} from "@material-ui/lab";
 import {CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {Skeleton} from "@material-ui/lab";
 
 function Dashboard() {
     const [loading, setLoading] = useState(true);
@@ -28,45 +28,55 @@ function Dashboard() {
     }
 
     return (
-            <Grid container spacing={4} style={{boxSizing:"border-box"}}>
-                {
-                    Object.entries(data).map(([key, value]) => {
-                        return (
-                            <Grid item lg={4} xl={4} key={key}>
-                                <Link to={value.path} style={{textDecoration: "none"}}>
-                                    <Card raised={true} className="flowCard">
-                                        <CardHeader style={{paddingBottom:"0"}} title={!loading ? value.text :
-                                            <Skeleton variant="text" width={250} height={32}/>}/>
-                                        <CardContent style={{paddingTop:"0"}}>
-                                            {!loading ? <h1>{value.number}</h1> :
-                                                <Skeleton variant="text" className="countSkeleton" width={32}
-                                                          height={64} />}
-                                        </CardContent>
-                                    </Card>
-                                </Link>
-                            </Grid>
+        <Grid container spacing={4}>
+            {!loading ?
+                Object.entries(data).map(([key, value]) => {
+                    return (
+                        <Grid item lg={4} xl={4} key={key}>
+                            <Link to={value.path} style={{textDecoration: "none"}}>
+                                <Card raised={true} className="flowCard">
+                                    <CardHeader style={{paddingBottom: "0"}} title={value.text}/>
 
-                        )
+                                    <CardContent style={{paddingTop: "0"}}>
+                                        {<h1>{value.number}</h1>}
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        </Grid>
 
-                    })
-                }
-                <Grid item xl={12} lg={12}>
-                    <Card raised={true} style={{marginBottom:"10px"}}>
-                        <CardContent>
-                    <h2 className="appointmentChartTitle">Appointment Count By Day of The Week</h2>
-                    <ResponsiveContainer width="100%" aspect={4}>
-                    <LineChart data={weekData}>
-                        <Line type="monotone" dataKey="count" stroke="#8884d8"/>
-                        <CartesianGrid stroke="#ccc"/>
-                        <XAxis dataKey="_id"/>
-                        <YAxis/>
-                        <Tooltip />
-                    </LineChart>
-                    </ResponsiveContainer>
-                        </CardContent>
-                    </Card>
+                    )
+
+                }) : <Grid container spacing={4}>
+
+                    {
+                        Array.from(Array(3), (i) => {
+                            return (
+                                <Grid item key={i} lg={4} xl={4}>
+                                    <Skeleton variant="rect" width={387.66 - 32} height={152 - 32}
+                                              style={{borderRadius: "15px", margin: "16px"}}/>
+                                </Grid>
+                            )
+                        })
+                    }
                 </Grid>
+            }
+            <Grid item xl={12} lg={12}>
+                <Card raised={true} style={{marginBottom: "10px"}}>
+                    <CardContent>
+                        <h2 className="appointmentChartTitle">Appointment Count By Day of The Week</h2>
+                        <ResponsiveContainer width="100%" aspect={4}>
+                            <LineChart data={weekData}>
+                                <Line type="monotone" dataKey="count" stroke="#8884d8"/>
+                                <CartesianGrid stroke="#ccc"/>
+                                <XAxis dataKey="_id"/>
+                                <YAxis/>
+                                <Tooltip/>
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </CardContent>
+                </Card>
             </Grid>
+        </Grid>
     )
 }
 
