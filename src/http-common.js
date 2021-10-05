8b273b80d3481a80f92a721116aa73ca8f86e736
AmanMU <69305642+AmanMU@users.noreply.github.com>
2021-10-05 19:50:36 +0530

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //You can change the baseURL that depends on REST APIs url that your Server configures.
 
 export default axios.create({
-  baseURL: "https://ispirithalei-api.herokuapp.com/",
+  baseURL: "https://ispirithalei-api.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
