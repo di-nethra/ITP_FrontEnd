f1b542a5618727f51022e580c9a641d53facd2e9
Asel Jayasooriya <80502785+AselJayasooriya@users.noreply.github.com>
2021-10-06 12:35:00 +0530

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default axios.create({
 
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://ispirithalei.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
